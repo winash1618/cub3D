e0fa0eaabbcaf53c5215dc291bc7882279b27555
winash1618 <asharafbarcalona786@gmail.com>
2022-07-25 06:25:20 +0400

    Hexagon drawn

--- Learning process/cub3d.c
+++ Learning process/cub3d.c
@@ -27,31 +27,29 @@ int	main(void)
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,&img.endian);
-	int x = 100;
-	int y = 500;
-
-	while (x < 500)
+	int x = 200;
+	int y = 300;
+	int z = 300;
+	while (x <= 600)
 	{
-		my_mlx_pixel_put(&img, x, 500, 0x00FF0000);
-		if (x <= 300)
-			my_mlx_pixel_put(&img, x, y--, 0x00FF0000);
-		if (x < 500 && x > 300)
+		if (x < 300)
+		{
 			my_mlx_pixel_put(&img, x, y++, 0x00FF0000);
+			my_mlx_pixel_put(&img, x, z--, 0x00FF0000);
+		}
+		if (x >= 300 && x <= 500)
+		{
+		my_mlx_pixel_put(&img, x, 400, 0x00FF0000);
+		my_mlx_pixel_put(&img, x, 200, 0x00FF0000);
+
+		}
+		if (x >500 )
+		{
+			my_mlx_pixel_put(&img, x, y--, 0x00FF0000);
+			my_mlx_pixel_put(&img, x, z++, 0x00FF0000);
+		}
 		x++;
 	}
-	// x = 100;
-	// while (x < 300)
-	// {
-	// 	my_mlx_pixel_put(&img, x, y, 0x00FF0000);
-	// 	x++;
-	// 	y--;
-	// }
-	// while (x < 500)
-	// {
-	// 	my_mlx_pixel_put(&img, x, y, 0x00FF0000);
-	// 	x++;
-	// 	y++;
-	// }
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
