4adceffd37610a3fc8717c2d5cf89b11a4b5c31a
Nass42 <namohamm@lab3r5s14.42abudhabi.ae>
2022-10-10 12:03:30 +0400

    push

--- parsing/ft_set_map.c
+++ parsing/ft_set_map.c
@@ -6,11 +6,29 @@
 /*   By: mkaruvan <namohamm@student.42.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 10:40:04 by mkaruvan          #+#    #+#             */
-/*   Updated: 2022/10/10 10:55:53 by mkaruvan         ###   ########.fr       */
+/*   Updated: 2022/10/10 11:12:31 by mkaruvan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+/*----------------------------------------------------*/
+int	ft_start_point(char *str)
+{
+	int	i;
+	int count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		if (str[i] == 'N' || str[i] == 'S' || str[i] == 'E' || str[i] == 'W')
+			count++;
+		i++;
+	}
+	return (count);
+}
+/*----------------------------------------------------*/
 
 /*----------------------------------------------------*/
 int	ft_is_linemap(char *tmp)
@@ -38,8 +56,10 @@ t_map	*ft_set_map(int fd, int *err)
 	char	*line;
 	char	*tmp;
 	t_map	*map;
+	int		count;
 
 	map = NULL;
+	count = 0;
 	while(1)
 	{
 		line = get_next_line(fd);
@@ -55,11 +75,17 @@ t_map	*ft_set_map(int fd, int *err)
 		if (ft_is_linemap(tmp))
 		{
 			printf("tmp: [%s]----{%d}\n", tmp, ft_is_linemap(tmp));
+			// ft_is_validlinemap(tmp, err);
+			count += ft_start_point(tmp);
 		}
+		else
+			*err = 4;
 		free(tmp);
 		free(line);
 		line = NULL;
 	}
+	if (count != 1)
+		*err = 3;
 	return(map);
 }
 /*----------------------------------------------------*/
